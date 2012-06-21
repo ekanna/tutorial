7a6d91f94e7bea4d3d947d391f8218486ab76f96
koti <koti@ekanna.com>
2012-06-22 00:36:46 +0530

    tested staging area

--- hello.js
+++ hello.js
@@ -3,3 +3,5 @@ var server = http.createServer(function(req, res){
   res.end('Hello World');
 })
 server.listne(3333, '127.0.0.1');
+console.log('server running at http://127.0.0.1:3333');
+console.log('hai');
